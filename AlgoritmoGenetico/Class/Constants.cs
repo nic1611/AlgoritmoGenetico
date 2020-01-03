d86fffe3678252f313d3b4c0057d9dae9c87fbb3
Nicolas <nisoffi@gmail.com>
2020-01-03 16:11:56 -0300

    Primeiro commit para master com o projeto AG rodando

---
+++ AlgoritmoGenetico/Class/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AlgoritmoGenetico.Class
+{
+    public abstract class Constants
+    {
+        static public int sizeCromossomo = 9;                               //Tamanho do Individuo
+        static public int sizePopulacao = 500;                              //Tamanho da populaçao
+        static public int functionXSize = (int)Math.Pow(2, sizeCromossomo); //Função como em 0 até 2 exponencial tamanho do cromossomo
+        public static Random random = new Random((int)DateTime.Now.Ticks);  //Objeto randomico, gera numeros pelo clock do processado
+
+        public static double function1(double x)
+        {
+             return 100 + Math.Abs(x * Math.Sin(Math.Sqrt(Math.Abs(x))));
+        }
+
+    }
+}
